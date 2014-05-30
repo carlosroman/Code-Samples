613975c8ba6fce332bea916fd352ba13444b951c
Carlos Roman <carlos.roman.ext@bbc.co.uk>
2014-05-30 17:54:20 +0100

    Added test to check that the JSON produced matches what we expected

--- camel/camel-scala-jackson/src/main/scala/com/carlosroman/samples/camel/jackson/domain/DomainObject.scala
+++ camel/camel-scala-jackson/src/main/scala/com/carlosroman/samples/camel/jackson/domain/DomainObject.scala
@@ -2,6 +2,12 @@ package com.carlosroman.samples.camel.jackson.domain
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-case class DomainObject(@JsonProperty("valueOne") valueOne: Option[String] = None) {
+//import com.fasterxml.jackson.annotation.JsonInclude
+//import com.fasterxml.jackson.annotation.JsonInclude.Include
+//import Include.NON_NULL
+
+//@JsonInclude(NON_NULL)
+case class DomainObject(@JsonProperty("valueOne") valueOne: Option[String] = None,
+                        @JsonProperty("values") values: Seq[String] = Nil) {
 
 }
--- camel/camel-scala-jackson/src/test/scala/com/carlosroman/samples/camel/jackson/SimpleJacksonRouteTest.scala
+++ camel/camel-scala-jackson/src/test/scala/com/carlosroman/samples/camel/jackson/SimpleJacksonRouteTest.scala
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory
 import org.apache.camel.impl.{SimpleRegistry, DefaultCamelContext}
 import org.apache.camel.component.mock.MockEndpoint
 import com.carlosroman.samples.camel.jackson.domain.DomainObject
+import org.json4s.DefaultFormats
 
 class SimpleJacksonRouteTest extends FunSuite with Matchers with BeforeAndAfter with OneInstancePerTest {
   def LOG = LoggerFactory.getLogger(classOf[SimpleJacksonRouteTest])
@@ -29,13 +30,23 @@ class SimpleJacksonRouteTest extends FunSuite with Matchers with BeforeAndAfter 
   }
 
   test("should create JSON from Scala Domain Object") {
-    val payload = DomainObject(Some("value one"))
+    val payload = DomainObject(Some("value one"), Seq("one", "two"))
+    val endpoint = getMockEndpoint(toUri)
+    endpoint.expectedMessageCount(1)
     template sendBody(marshalFromUri, payload)
+    endpoint.assertIsSatisfied()
+    val body = endpoint.getExchanges.get(0).getIn.getBody(classOf[String])
+    LOG.info("Got Body of '{}'", body)
+    import org.json4s._
+    import org.json4s.jackson.JsonMethods._
+    implicit lazy val formats = DefaultFormats
+    val actual = parse(body).extract[DomainObject]
+    actual should equal(payload)
   }
 
 
   test("should create Scala Domain Object from JSON") {
-    val payload = DomainObject(Some("value one"))
+    val payload = DomainObject(Some("value one"), Seq("one", "two"))
     val endpoint = getMockEndpoint(toUri)
     endpoint.expectedBodiesReceived(payload)
     template sendBody(unmarshalFromUri, domainObjectJson)
@@ -43,6 +54,6 @@ class SimpleJacksonRouteTest extends FunSuite with Matchers with BeforeAndAfter 
   }
 
   def domainObjectJson = """{
-  "valueOne": "value one"
+  "valueOne": "value one", "values":["one", "two"]
   }"""
 }
